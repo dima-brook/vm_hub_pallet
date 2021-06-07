62bccea124c25804e70974220243d345d8585eab
VKint <dima@xp.network>
2021-06-07 15:22:04 +0530

    runtime: set spec_version to 100

--- substrate-node-template/runtime/src/lib.rs
+++ substrate-node-template/runtime/src/lib.rs
@@ -101,10 +101,10 @@ pub mod opaque {
 }
 
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-	spec_name: create_runtime_str!("node-frontier-template"),
-	impl_name: create_runtime_str!("node-frontier-template"),
+	spec_name: create_runtime_str!("node-template"),
+	impl_name: create_runtime_str!("node-template"),
 	authoring_version: 1,
-	spec_version: 1,
+	spec_version: 100,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
